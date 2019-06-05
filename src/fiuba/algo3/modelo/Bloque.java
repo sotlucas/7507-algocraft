ecbc4c51e207f012bdd0b99c15d8e752384a2aa7
Lucas Sotelo <lucasnsotelog@gmail.com>
2019-06-05 15:09:19 -0300

    Elimino atributo desgaste de Bloque

--- src/fiuba/algo3/modelo/Bloque.java
+++ src/fiuba/algo3/modelo/Bloque.java
@@ -3,8 +3,7 @@ package fiuba.algo3.modelo;
 public abstract class Bloque {
 
     protected int durabilidad;
-    protected Desgaste desgaste;
-    
+
     public int getDurabilidad() {
         return this.durabilidad;
     }
