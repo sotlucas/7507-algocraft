3f28f68465f7f2ecacfe3be747ac802b5b953065
Lucas Sotelo <lucasnsotelog@gmail.com>
2019-06-09 17:07:17 -0300

    Fusiono las clases Personaje y Jugador

--- src/fiuba/algo3/modelo/Jugador.java
+++ src/fiuba/algo3/modelo/Jugador.java
@@ -1,15 +1,34 @@
 package fiuba.algo3.modelo;
 
-public class Jugador {
+public class Jugador implements Elemento {
 
     Inventario inventario;
+    Posicion posicion;
 
     public Jugador() {
         this.inventario = new Inventario();
+        this.posicion = new Posicion();
         this.inventario.agregarHerramienta(new Hacha(new Madera()));
     }
 
     public boolean inventarioContiene(Herramienta herramienta) {
         return this.inventario.contiene(herramienta);
+    }
+
+    public void avanzar(Mapa mapa, Direccion direccion) {
+        direccion.avanzar(mapa, this);
+    }
+
+    public void setPosicion(int x, int y) {
+        this.posicion.setX(x);
+        this.posicion.setY(y);
+    }
+
+    public int getPosicionX() {
+        return this.posicion.getX();
+    }
+
+    public int getPosicionY() {
+        return this.posicion.getY();
     }
 }
--- test/fiuba/algo3/modelo/JugadorTest.java
+++ test/fiuba/algo3/modelo/JugadorTest.java
@@ -1,13 +1,19 @@
 package fiuba.algo3.modelo;
 
 import org.junit.Test;
-
 import org.junit.Assert;
 
 public class JugadorTest {
 
     @Test
-    public void test01JugadorRecienCreadoIniciaConHachaDeMaderaEnElInventario() {
+    public void testAlCrearJugadorEsDistintoDeNull(){
+        Jugador jugador = new Jugador();
+
+        Assert.assertNotNull(jugador);
+    }
+
+    @Test
+    public void testJugadorRecienCreadoIniciaConHachaDeMaderaEnElInventario() {
         Jugador jugador = new Jugador();
         Madera madera = new Madera();
         Hacha hachaDeMadera = new Hacha(madera);
