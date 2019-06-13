d4527a3fcb3a59e96defdc862b929678a411b676
Lucas Sotelo <lucasnsotelog@gmail.com>
2019-06-13 14:14:46 -0300

    Agrego relacion de crafteo entre Jugador y MesaCrafteo

--- src/fiuba/algo3/modelo/Inventario.java
+++ src/fiuba/algo3/modelo/Inventario.java
@@ -5,16 +5,22 @@ import java.util.ArrayList;
 public class Inventario {
 
     private ArrayList<Herramienta> herramientas;
+    private ArrayList<Material> materiales;
 
     public Inventario() {
         this.herramientas = new ArrayList<>();
+        this.materiales = new ArrayList<>();
     }
 
     public void agregarHerramienta(Herramienta herramienta) {
         this.herramientas.add(herramienta);
     }
 
-    public boolean contiene(Herramienta herramienta) {
+    public void agregarMaterial(Material material) {
+        this.materiales.add(material);
+    }
+
+    public boolean contieneHerramienta(Herramienta herramienta) {
 
         int indice = 0;
         boolean herramientaEncontrada = false;
@@ -31,4 +37,27 @@ public class Inventario {
 
         return herramientaEncontrada;
     }
+
+    public boolean contieneMaterial(Material material) {
+
+        int indice = 0;
+        boolean materialEncontrado = false;
+
+        while((!materialEncontrado) && (indice < materiales.size())){
+
+            if (material.getClass() == this.materiales.get(indice).getClass()) {
+                materialEncontrado = true;
+            }
+
+            indice++;
+
+        }
+
+        return materialEncontrado;
+    }
+
+    public Material seleccionarMaterial(int posicion) {
+        return materiales.get(posicion);
+    }
+
 }
