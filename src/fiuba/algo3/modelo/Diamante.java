dd8ed1d882be1e9842b91da637b7d703aff9af01
AgustinHejeij <agustin.hejeij@gmail.com>
2019-06-20 13:33:39 -0300

    Correccion en material diamante

--- src/fiuba/algo3/modelo/Diamante.java
+++ src/fiuba/algo3/modelo/Diamante.java
@@ -1,7 +1,5 @@
 package fiuba.algo3.modelo;
 
 public class Diamante extends Material{
-    public  Madera() {
-        this.identificador = 'd';
-    }
+    public Diamante() { this.identificador = 'd'; }
 }
